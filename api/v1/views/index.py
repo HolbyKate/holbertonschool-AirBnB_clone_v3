e5d03102c64f9be226c000cd419be2f2e582d11d
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-12-20 12:55:55 -0800

    use the newly added count() method from storage

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,6 +7,20 @@ from flask import jsonify
 from models import storage
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route("/status", methods=["GET"], strict_slashes=False)
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def storage_counts():
+    """use the newly added count() method from storage"""
+    cls_counts = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(cls_counts)
