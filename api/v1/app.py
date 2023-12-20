61d9e0e89bf5ec80f8a9cc2e214b510b76e72409
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-12-20 06:54:14 -0800

    declare a method to handle @app.teardown_appcontext

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python3
-"""_summary_"""
+""" Flask app"""
 
 from models import storage
 from api.v1.views import app_views
-from flask import jsonify, abort, request
+from flask import Flask
+import os
 
 
 app = Flask(__name__)
-CORS(app, origins="0.0.0.0")
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def tear_down(self):
-    '''
-    close query after each session
-    '''
+def teardown_appcontext(exception):
     storage.close()
 
 
-@app.errorhandler(404)
-def not_found(error):
-    '''
-    return JSON formatted 404 status code response
-    '''
-    return make_response(jsonify({'error': 'Not found'}), 404)
-
-
 if __name__ == "__main__":
-    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
-            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
