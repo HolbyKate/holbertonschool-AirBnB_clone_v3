0f136f1f6dfc3edfeaa806186608fbdfee422eab
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-12-20 13:47:03 -0800

    Fix with module documented

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """
-Create a new view for State objects that handles 
-all default RESTFul API actions
+Create a new view for State objects that handles all default
+RESTFul API actions
 """
-
 
 from api.v1.views import app_views
 from flask import jsonify, abort, request
