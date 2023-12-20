43d3fc13b912fd9e6641f11c9ef39fce059286b5
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-12-20 13:15:36 -0800

    Fix handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -27,11 +27,14 @@ def not_found(e):
     return response
 
 
+class Handler:
+    def not_found(error):
+        response = jsonify({"error": "Not found"})
+        response.status_code = 404
+        return response
+
+
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
-
-
-    r = requests.get("http://0.0.0.0:5000/api/v1/notexist")
-    print(r.json().get("error"))
